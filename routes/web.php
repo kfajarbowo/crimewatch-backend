28337d2d480a590b6a2fb87791f17d9e32e14acf
Kidung Fajar Prabowo <kfajarbowo12@gmail.com>
2025-10-22 13:46:29 +0700

    chore: add route SEO

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,19 @@ use App\Http\Controllers\Admin\NewsController;
 use App\Http\Controllers\Admin\VideoController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\SitemapController;
 use Illuminate\Support\Facades\Route;
 
 // Frontend Routes
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/news/{slug}', [HomeController::class, 'show'])->name('news.detail');
 Route::get('/category/{slug}', [HomeController::class, 'category'])->name('category.detail');
+
+// Sitemap Routes
+Route::get('/sitemap.xml', [SitemapController::class, 'index']);
+Route::get('/sitemap-news.xml', [SitemapController::class, 'news']);
+Route::get('/sitemap-categories.xml', [SitemapController::class, 'categories']);
+Route::get('/sitemap-index.xml', [SitemapController::class, 'sitemapIndex']);
 
 // Auth Routes
 Route::middleware('guest')->group(function () {
@@ -28,11 +35,9 @@ Route::middleware('auth')->group(function () {
         Route::resource('categories', CategoryController::class);
         Route::resource('news', NewsController::class);
         Route::resource('videos', VideoController::class);
-        
-        // Manual publish scheduled news
+
+        // Manual publish
         Route::post('news/publish-scheduled', [NewsController::class, 'publishScheduled'])->name('news.publish-scheduled');
-        // Test scheduler functionality
-        Route::post('news/test-scheduler', [NewsController::class, 'testScheduler'])->name('news.test-scheduler');
     });
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
